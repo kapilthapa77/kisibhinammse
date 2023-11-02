47c7dff1fb59e1ca722217ee74847b438e044653
Kapil Thapa <thapakapil128@gmail.com>
2023-11-02 17:00:09 +0530

    initial commit

---
+++ copy.js
@@ -0,0 +1,26 @@
+const http=require('http');
+const server= http.createServer( 
+    createServerCallbackFunction
+)
+let a=[];
+function createServerCallbackFunction(req, res){
+    const url= req.url;
+    if (url == '/create'){
+    a.push(1);
+    res.write(JSON.stringify(a));
+    }
+    else if(url== '/update'){
+    a.push(1);
+    a[0] = 2;
+    res.write(JSON.stringify(a));
+    }
+    else if(url=='/read'){ 
+        res.write(JSON.stringify(a));
+    }
+    else if(url =='/delete'){
+    a.pop();
+    }
+    res.end();
+}
+server.listen(3000, function(){ console.log("Server Listening.."); 
+});
---
+++ indi.js
@@ -0,0 +1,18 @@
+const http=require("http");
+
+const server=http.createServer(callback);
+function callback(req,res){
+    let a=[50,23];
+    if(req.method=="POST"){
+        arguments.push(1);
+    }
+    else if(req.methode=="PUT"){
+        a[1]=2;
+    }
+    else if(req.method=="DELETE"){
+        a.pop();
+    }
+    res.write(JSON.stringify(a));
+    res.end();
+}
+server.listen(4000);
